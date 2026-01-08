6746960c2e1c1b7c615b4d5dd4bfb906f27d1c1a
Greg Maka <makagreg@yahoo.com>
2026-01-08 13:53:23 -0600

    Test

--- client/src/components/Auth/__tests__/Login.spec.tsx
+++ client/src/components/Auth/__tests__/Login.spec.tsx
@@ -95,7 +95,7 @@ const setup = ({
       isFetching={useGetStartupConfigReturnValue.isFetching}
       error={null}
       startupConfigError={null}
-      header={'Welcome back'}
+      header={'Wilmer/LibreChat'}
       pathname="login"
     >
       <Login />
